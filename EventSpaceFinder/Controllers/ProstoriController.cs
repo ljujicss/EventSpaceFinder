da7933b827948dbe0ade15cbf682f621087f68c5
Saska Ljujic <ljujicsaska@gmail.com>
2026-06-02 15:40:14 +0200

    Implementirana pretraga i rezervacija prostora

---
+++ EventSpaceFinder/Controllers/ProstoriController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventSpaceFinder.Controllers
+{
+    public class ProstoriController : Controller
+    {
+        private EventSpaceFinderEntities db = new EventSpaceFinderEntities();
+
+        public ActionResult Index(string search)
+        {
+            var prostori = db.Prostors.AsQueryable();
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                prostori = prostori.Where(p =>
+                    p.naziv.Contains(search) ||
+                    p.opis.Contains(search) ||
+                    p.adresa.Contains(search) ||
+                    p.tip_prostora.Contains(search) ||
+                    p.Grad.naziv_grada.Contains(search)
+                );
+            }
+
+            ViewBag.Search = search;
+
+            return View(prostori.ToList());
+        }
+
+
+        public ActionResult DetaljnaPretraga()
+        {
+            ViewBag.id_grada = new SelectList(db.Grads, "id_grada", "naziv_grada");
+            ViewBag.id_tipa_dogadjaja = new SelectList(db.TipDogadjajas, "id_tipa_dogadjaja", "naziv_tipa");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DetaljnaPretraga(int? id_grada, int? id_tipa_dogadjaja, int? broj_gostiju, decimal? budzet, DateTime? datum_dogadjaja)
+        {
+            var prostori = db.Prostors.AsQueryable();
+
+            if (id_grada != null)
+            {
+                prostori = prostori.Where(p => p.id_grada == id_grada);
+            }
+
+            if (broj_gostiju != null)
+            {
+                prostori = prostori.Where(p => p.kapacitet >= broj_gostiju);
+            }
+
+            if (budzet != null)
+            {
+                prostori = prostori.Where(p => p.Pakets.Any(pk => pk.cijena <= budzet));
+            }
+
+            if (id_tipa_dogadjaja != null)
+            {
+                prostori = prostori.Where(p => p.TipDogadjajas.Any(t => t.id_tipa_dogadjaja == id_tipa_dogadjaja));
+            }
+
+            if (datum_dogadjaja != null)
+            {
+                prostori = prostori.Where(p => !p.KalendarZauzetostis.Any(k =>
+                    k.datum == datum_dogadjaja &&
+                    (k.status_datuma == "Zauzeto" || k.status_datuma == "Na cekanju")
+                ));
+            }
+
+            ViewBag.id_grada = new SelectList(db.Grads, "id_grada", "naziv_grada", id_grada);
+            ViewBag.id_tipa_dogadjaja = new SelectList(db.TipDogadjajas, "id_tipa_dogadjaja", "naziv_tipa", id_tipa_dogadjaja);
+
+            return View("RezultatiPretrage", prostori.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            Prostor prostor = db.Prostors.Find(id);
+
+            if (prostor == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(prostor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EventSpaceFinder/Controllers/RezervacijeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace EventSpaceFinder.Controllers
+{
+    public class RezervacijeController : Controller
+    {
+        private EventSpaceFinderEntities db = new EventSpaceFinderEntities();
+
+        public ActionResult Create(int id_prostora)
+        {
+            Prostor prostor = db.Prostors.Find(id_prostora);
+
+            if (prostor == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.id_prostora = id_prostora;
+            ViewBag.naziv_prostora = prostor.naziv;
+            ViewBag.id_paketa = new SelectList(db.Pakets.Where(p => p.id_prostora == id_prostora && p.aktivan == true), "id_paketa", "naziv_paketa");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int id_prostora, int id_paketa, DateTime datum_dogadjaja, int broj_gostiju, string napomena)
+        {
+            int id_korisnika = 2;
+
+            db.Database.ExecuteSqlCommand(
+                "EXEC sp_DodajRezervaciju @id_korisnika, @id_prostora, @id_paketa, @datum_dogadjaja, @broj_gostiju, @napomena",
+                new SqlParameter("@id_korisnika", id_korisnika),
+                new SqlParameter("@id_prostora", id_prostora),
+                new SqlParameter("@id_paketa", id_paketa),
+                new SqlParameter("@datum_dogadjaja", datum_dogadjaja),
+                new SqlParameter("@broj_gostiju", broj_gostiju),
+                new SqlParameter("@napomena", napomena ?? "")
+            );
+
+            return RedirectToAction("Index", "Prostori");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
